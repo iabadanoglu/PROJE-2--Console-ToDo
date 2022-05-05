e6e887e901547d4916b57efeca493cfbc97c25c0
İbrahim ABADANOĞLU <55638645+iabadanoglu@users.noreply.github.com>
2022-05-06 01:10:04 +0300

    Update TodoItem.cs

--- PROJE-2 -Console-ToDo/TodoItem.cs
+++ PROJE-2 -Console-ToDo/TodoItem.cs
@@ -2,18 +2,6 @@
 {
     class TodoItem
     {
-        public TodoItem(string text, int number)
-        {
-            Text = text;
-            Number = number;
-        }
-
-        public string Text { get; }
-        public int Number { get; }
-
-        public override string ToString()
-        {
-            return "#" + Number + " " + Text;
-        }
+        
     }
 }
